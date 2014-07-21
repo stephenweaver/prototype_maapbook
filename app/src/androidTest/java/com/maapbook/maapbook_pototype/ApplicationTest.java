db0b71ba81f2544324a6c53da07dac808ff56310
stephen.weaver <sjwsgm@gmail.com>
2014-07-21 18:31:38 -0400

    Init commit

---
+++ app/src/androidTest/java/com/maapbook/maapbook_pototype/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.maapbook.maapbook_pototype;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
